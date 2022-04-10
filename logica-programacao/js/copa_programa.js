5a0dbca7ccb6d5aacf6acad47df84ab521465d45
Maicon Jordan <maiconplus@gmail.com>
2022-04-10 15:43:01 -0300

    repetição

---
+++ logica-programacao/js/copa_programa.js
@@ -0,0 +1,17 @@
+function pulaLinha(){
+    document.write("<br>")
+}  
+
+function mostra (msg){
+document.write(msg);      
+}
+
+
+var anoCopa = parseInt(prompt("primeiro ano?"))
+var anoAtual = 2022
+var limite = parseInt(prompt("Digite até que ano você quer a copa?"))
+
+for(anoCopa = anoCopa ; anoCopa <= limite ; anoCopa += 4){
+    mostra(anoCopa);
+    pulaLinha();
+}
