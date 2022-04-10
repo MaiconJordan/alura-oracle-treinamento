8240dcb538cfcee4b4064b121e89b20ac0ea22d7
Maicon Jordan <maiconplus@gmail.com>
2022-04-09 21:19:58 -0300

    refatoração do adivinha

--- logica-programacao/js/adivinha_programa.js
+++ logica-programacao/js/adivinha_programa.js
@@ -6,7 +6,7 @@ function mostra (msg){
 document.write(msg);      
 }
 
-var numero = Math.round( Math.random() * 10);
+/* var numero = Math.round( Math.random() * 10);
 
 var chute = parseInt(prompt("Digite um número:"));
 
@@ -18,4 +18,37 @@ if(numero == chute ){
     mostra("Que pena! você errou.")
     pulaLinha();
     mostra("Número correto é: " + numero)
+} */
+
+// Exercicio de Refatoração 
+
+function sorteia(n) {
+    
+    return Math.round(Math.random() * n)  ;
+
+    
 }
+
+var numeroSorteado = sorteia(10);
+var chute = parseInt(prompt("Chute um número"));
+
+if(numeroSorteado == chute){
+    mostra("Parabéns você acertou o número sorteado")
+    pulaLinha();
+    mostra("Número " + numeroSorteado);
+
+} else {
+    mostra("Você errou");
+    pulaLinha();
+
+    if(numeroSorteado > chute){
+        mostra("Número é maior que o seu chute")
+    } else {
+        mostra("Número menor que seu chute");
+    }
+}
+
+
+
+
+
