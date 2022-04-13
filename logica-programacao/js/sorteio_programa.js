ed486892d7c81c40f5a3043f7f6c635efa10e4b3
Maicon Jordan <maiconplus@gmail.com>
2022-04-12 23:41:30 -0300

    refatoração sorteio

--- logica-programacao/js/sorteio_programa.js
+++ logica-programacao/js/sorteio_programa.js
@@ -1,3 +1,4 @@
+/*
 function pulaLinha(){
     document.write("<br>")
 }  
@@ -22,4 +23,48 @@ for(i = 0 ; i < tentativas ; i++){
     }
 }
 
-mostra(numeroSorteado)
+mostra(numeroSorteado) 
+
+
+*/
+
+
+
+
+
+
+    function pulaLinha() {
+
+        document.write("<br>");
+        document.write("<br>");
+}
+
+    function mostra(frase) {
+
+        document.write(frase);
+        pulaLinha();
+}
+
+var numeroPensado = Math.round(Math.random() * 10);
+
+var tentativas = 1;
+
+while(tentativas <= 3) {
+
+    var chute = parseInt(prompt("Digite seu chute!"));
+
+    if(chute == numeroPensado) {
+
+        mostra("Você ACERTOU, o número pensado era " + numeroPensado);
+        break;
+
+    } else {
+
+        mostra("Você ERROU!");
+    }
+
+    tentativas++;
+}
+
+mostra("FIM");
+
