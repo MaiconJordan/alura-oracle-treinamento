458fc7b6c20c4b0a4493c3fa76be85e496252fdd
Maicon Jordan <maiconplus@gmail.com>
2022-04-29 23:48:54 -0300

    refatorando

--- aula-javascript/introducao-javascript/js/principal.js
+++ aula-javascript/introducao-javascript/js/principal.js
@@ -1,36 +1,37 @@
+var pacientes = document.querySelectorAll(".paciente");
 
+for (var i = 0; i < pacientes.length; i++) {
 
+    var paciente = pacientes[i];
 
-
-var paciente = document.querySelectorAll(".paciente");
-
-for(i = 0; i < paciente.length; i++){
-
-    var tdPeso = paciente[i].querySelector(".info-peso");
+    var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
-    var tdAltura = paciente[i].querySelector(".info-altura")
+
+    var tdAltura = paciente.querySelector(".info-altura");
     var altura = tdAltura.textContent;
-    var tdIMC = paciente[i].querySelector(".info-imc")
-    
-    var pesoValido = true;
-    var alturaValida = true;
-    
-    if(peso <= 0 || peso >= 500){
-        pesoValido = false;
-        tdIMC.textContent = 'Peso inválido'
-    
-    } else if (altura <= 0 || altura >= 3.00){
-        alturaValida = false;
-        tdIMC.textContent = 'Altura inválida'
-    } else {
-        var imc = peso / (altura * altura);
-        tdIMC.textContent = imc.toFixed(2);
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.style.color = "red"
     }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+    }
+
+  if (alturaEhValida && pesoEhValido) {
     
-
-
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+   
+    }
 }
-
-
-
-
